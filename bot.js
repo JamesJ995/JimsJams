e89a2d8e61df6e73d02ee1d3f6d2d87b6c986afc
jamesj995 <mrjam7@gmail.com>
2021-08-31 18:09:17 -0500

    token working

--- bot.js
+++ bot.js
@@ -1,1 +1,18 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const readyDiscord = () => {
+  return console.log('beep beep boop bop');
+};
+
+// When the client is ready, run this code (only once)
+client.on('ready', readyDiscord);
+
+// Login to Discord with your client's token
+client.login(token);
+
 console.log('Beep Boop');
