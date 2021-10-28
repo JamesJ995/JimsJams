3908ae52476597222edb22c3902f53b0ef464cec
JamesJ995 <mrjam7@gmail.com>
2021-10-28 16:16:50 -0500

    slash commands working

--- bot.js
+++ bot.js
@@ -1,24 +1,72 @@
-require('dotenv').config(); //initialize dotenv
-const { Client, Intents } = require("discord.js");
+require("dotenv").config(); //initialize dotenv
+const fs = require("fs");
+const { Client, Intents, Collection } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const TOKEN = process.env.TOKEN;
+const GUILD_ID = process.env.GUILD_ID;
+const CLIENT_ID = process.env.CLIENT_ID;
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.on("ready", () => {
-  console.log("Jim's Jam's Loaded!");
-});
+const commands = [];
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
-client.on("messageCreate", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  } else
+// Creating a collection for commands in client
+client.commands = new Collection();
 
-    if (message.content.startsWith("foo")) {
-      message.channel.send("bar!");
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+  client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, this only runs once
+client.once("ready", () => {
+  console.log("Jims Jams is Ready!");
+  // Registering the commands in the client
+  const CLIENT_ID = client.user.id;
+  const rest = new REST({
+    version: "9",
+  }).setToken(TOKEN);
+  (async () => {
+    try {
+      if (!GUILD_ID) {
+        await rest.put(Routes.applicationCommands(CLIENT_ID), {
+          body: commands,
+        });
+        console.log("Successfully registered application commands globally");
+      } else {
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+          body: commands,
+        });
+        console.log(
+          "Successfully registered application commands for Honking at Blizz"
+        );
+      }
+    } catch (error) {
+      if (error) console.error(error);
     }
+  })();
 });
 
-client.login(process.env.TOKEN);
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    if (error) console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
 
-
+client.login(TOKEN);
